8b3b4fe34e3b3c36032aada547a77fc860f64532
Kristian B <KrisZane@users.noreply.github.com>
2016-04-14 12:39:58 +0000

    [FEAT] Add the ability to use webhooks

--- server.js
+++ server.js
@@ -1,39 +1,102 @@
 "use strict";
-/*let BotApi = new (require('./lib/telegrambotapi').BotApi)({"botToken": "1122"});
-let BotManager = new (require('./lib/telegrambotmanager').BotManager)({"botToken": "1122"});*/
-//mainlet botData = {};
-//let main = {};
-
 class BotManager {
   constructor(settings) {
-    //main = this;
+    this.lib = {};
+    this.lib.fs = require('fs');
+    this.lib.https = require('https');
+    this.lib.multipartGenerate = new (require('multipart-light').generate);
+    
     this.data = {
       "updateCount": 0
       , "users": {
       }
     };
+    
     this.options = {
-      "baseUrl": "https://api.telegram.org/bot"
+      "baseUrl": "https://api.telegram.org/bot" + settings.botToken + '/'
       , "updateTime": 3000
       , "apiCalls": {
         "sendMessage": "sendMessage?chat_id=${chatId}&text=${encodedMessage}"
         , "getUpdates": "getUpdates?offset=${offset}"
+        , "setWebhook": "setWebhook"
       }
     };
+    if(settings.key !== undefined && settings.cert !== undefined) {
+      this.options.key = this.lib.fs.readFileSync(settings.key);
+      this.options.cert = this.lib.fs.readFileSync(settings.cert);
+    }
+    else {
+      settings.type = 'poller';
+    };
+    
     this.functionReferenceStore = {};
-
-    this.https = require('https');
-
-    this.options.baseUrl += settings.botToken + '/';
-    setInterval(() => {this.watchUpdates();}, this.options.updateTime);
-    this.watchUpdates();
-
+    
+    if(settings.type === 'webhook') {
+      this.createServer();
+      this.registerServer();
+    }
+    else {
+      setInterval(() => {this.watchUpdates();}, this.options.updateTime);
+    };
+    
     return false;
+  }
+  createServer() {
+    this.lib.https.createServer(this.options, (req, res) => {
+      let data = '';
+      req.on('data', (chunk) => {
+        data += chunk;
+      });
+      req.on('end', () => {
+        console.log('recieved:');
+        console.log(data);
+        res.writeHead(200);
+        res.end("hello world\n");
+      });
+    }).listen(8080);
+  }
+  registerServer() {
+    let multipartData = [
+      {"data": "https://botmanager.nexination.com/", "name": "url"}
+      , {"mimeType": "application/x-x509-ca-cert", "data": this.options.cert, "name": "certificate"}
+    ];
+    let multipartRequest = this.lib.multipartGenerate.request(multipartData);
+    multipartRequest.headers['Host'] = 'api.telegram.org';
+    multipartRequest.headers['User-Agent'] = 'Node.JS';
+    multipartRequest.headers['Accept-Encoding'] = 'gzip,deflate';
+    
+    console.log(multipartRequest.headers);
+    console.log(multipartRequest.body.toString());
+    
+    let request = this.lib.https.request({
+      hostname: 'api.telegram.org',
+      port: 443,
+      path: this.options.baseUrl + 'setWebhook',
+      method: 'POST',
+      headers: multipartRequest.headers
+    });
+    
+    request.on('error', function (error) {
+      console.log(error);
+    });
+    request.on('response', function (response) {
+      let data = '';
+      response.on('data', function (chunk) {
+        data += chunk;
+      });
+      response.on('end', function () {
+        console.log('EndRequest:');
+        console.log(data);
+      });
+    });
+    
+    request.write(multipartRequest.body);
+    request.end();
   }
   watchUpdates() {
     console.log('Checked for updates.');
     this.apiCall('getUpdates', {"offset": this.data.updateCount});
-
+    
     return false;
   }
   apiCall(call, data) {
@@ -46,17 +109,17 @@ class BotManager {
         , function(replacement) {
           let replaced = '';
           replacement = replacement.substr(2, (replacement.length -3));
-
+          
           if(data.hasOwnProperty(replacement)) {
             replaced = encodeURIComponent(data[replacement]);
           };
-
+          
           return replaced;
         }
       );
-
+      
       // Call the API
-      this.https.get(this.options.baseUrl + apiToCall, (resource) => {this.apiReturn(resource);}).on('error', function(e) {
+      this.lib.https.get(this.options.baseUrl + apiToCall, (resource) => {this.apiReturn(resource);}).on('error', function(e) {
         console.error(e);
       });
     };
@@ -68,21 +131,21 @@ class BotManager {
     let dataCount = 0;
     let fullData = '';
     let dataLength = parseInt(resource.headers['content-length']);
-
+    
     resource.on('data', function(data) {
       if(dataCount !== dataLength) {
-          fullData += data;
-          dataCount += data.length;
+        fullData += data;
+        dataCount += data.length;
       };
       if(dataCount === dataLength) {
         let jsonData = {};
         try {
-            jsonData = JSON.parse(fullData);
+          jsonData = JSON.parse(fullData);
         }
         catch(error) {
-            console.log(error);
+          console.log(error);
         };
-
+        
         // Checks if the API call was a success
         if(jsonData.hasOwnProperty('ok') && jsonData.ok && jsonData.result.hasOwnProperty('length')) {
           let resultLength = jsonData.result.length
@@ -94,7 +157,7 @@ class BotManager {
               let functionCall = result.message.text;
               functionCall = (functionCall.indexOf('/') === 0 ? functionCall.substr(1) : '');
               functionCall = (functionCall.indexOf('@') >= 0 ? functionCall.substr(0, functionCall.indexOf('@')) : functionCall);
-
+              
               // Stores the next update_id
               if(main.data.updateCount <= result.update_id) {
                 main.data.updateCount = result.update_id + 1;
@@ -123,7 +186,7 @@ class BotManager {
   }
   on(functionName, functionReference) {
     this.functionReferenceStore[functionName] = functionReference;
-
+    
     return false;
   }
   deferAction(chatId, action) {
